a862ff12e82d16884ea9d8c186603d1dfbac9f1b
erik <erik.rodrigu3z@gmail.com>
2020-08-14 03:11:43 -0600

    x

--- Fechas/Program.cs
+++ Fechas/Program.cs
@@ -11,25 +11,25 @@ namespace Fechas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fecha actual");
-            Console.WriteLine(DateTime.Now + "\r\n");
-            Console.WriteLine("Agrega 24 horas");            
-            Console.WriteLine(DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59));
+            //Console.WriteLine("Fecha actual");
+            //Console.WriteLine(DateTime.Now + "\r\n");
+            //Console.WriteLine("Agrega 24 horas");            
+            //Console.WriteLine(DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59));
 
-            DateTime date1 = new DateTime(2020, 4, 29, 16, 28, 0); //"4/30/2020 4:26:24 PM"; //DateTime.Now;
-            DateTime date2 = new DateTime(2020, 4, 29, 16, 27, 0); //DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
-            DateTime date3 = DateTime.UtcNow;
+            //DateTime date1 = new DateTime(2020, 4, 29, 16, 28, 0); //"4/30/2020 4:26:24 PM"; //DateTime.Now;
+            //DateTime date2 = new DateTime(2020, 4, 29, 16, 27, 0); //DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+            //DateTime date3 = DateTime.UtcNow;
 
 
-            if (date1 <= date2)
-            {
-                Console.WriteLine(date1 + " es menor que " + date2);
-            }
-            else
-            {
-                Console.WriteLine(date1 +" es mayor que " + date2);
+            //if (date1 <= date2)
+            //{
+            //    Console.WriteLine(date1 + " es menor que " + date2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(date1 +" es mayor que " + date2);
 
-            }
+            //}
             
             //Console.WriteLine(Assembly.GetEntryAssembly().FullName);
             //Console.WriteLine(DateTime.Now.ToUniversalTime().ToString("R"));
@@ -64,21 +64,47 @@ namespace Fechas
             Console.WriteLine(aDate.ToString("HH:mm:ss"));
             Console.WriteLine(aDate.ToString("yyyy MMMM"));
 
-            string sFecha = "10/05/2020 15:10:26";
-            DateTime dateconvert = Convert.ToDateTime(sFecha);
-            Console.WriteLine(dateconvert);
-            TimeSpan CurrentTime = DateTime.Now.TimeOfDay;
-            Console.WriteLine(CurrentTime);
-            Console.WriteLine(Convert.ToDateTime("12:14:30.2084468"));
-            DateTime finalTime = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
-            Console.WriteLine(finalTime + " date final");
-            Console.WriteLine(finalTime.TimeOfDay + " TimeSpan");
-            TimeSpan oTS = new TimeSpan(finalTime.Ticks);
-            Console.WriteLine(oTS + " TimeSpan final");
 
-            Console.WriteLine(DateTime.Now.TimeOfDay + " +24");           
-            
-            Console.WriteLine(DateTime.Now.ToLocalTime());
+// DateTime.Now.ToString("MM/dd/yyyy")     05 / 29 / 2015
+// DateTime.Now.ToString("dddd, dd MMMM yyyy")     Friday, 29 May 2015
+// DateTime.Now.ToString("dddd, dd MMMM yyyy")     Friday, 29 May 2015 05:50
+// DateTime.Now.ToString("dddd, dd MMMM yyyy")     Friday, 29 May 2015 05:50 AM
+// DateTime.Now.ToString("dddd, dd MMMM yyyy")     Friday, 29 May 2015 5:50
+// DateTime.Now.ToString("dddd, dd MMMM yyyy")     Friday, 29 May 2015 5:50 AM
+// DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss")    Friday, 29 May 2015 05:50:06
+// DateTime.Now.ToString("MM/dd/yyyy HH:mm")   05 / 29 / 2015 05:50
+// DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")    05 / 29 / 2015 05:50 AM
+// DateTime.Now.ToString("MM/dd/yyyy H:mm")    05 / 29 / 2015 5:50
+// DateTime.Now.ToString("MM/dd/yyyy h:mm tt")     05 / 29 / 2015 5:50 AM
+// DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")    05 / 29 / 2015 05:50:06
+// DateTime.Now.ToString("MMMM dd")    May 29
+// DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK")     2015 - 05 - 16T05: 50:06.7199222 - 04:00
+// DateTime.Now.ToString("ddd, dd MMM yyy HH’:’mm’:’ss ‘GMT’")     Fri, 16 May 2015 05:50:06 GMT
+// DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss")  2015 - 05 - 16T05: 50:06
+// DateTime.Now.ToString("HH:mm")  05:50
+// DateTime.Now.ToString("hh:mm tt")   05:50 AM
+// DateTime.Now.ToString("H:mm")   5:50
+// DateTime.Now.ToString("h:mm tt")    5:50 AM
+// DateTime.Now.ToString("HH:mm:ss")   05:50:06
+// DateTime.Now.ToString("yyyy MMMM")  2015 May
+
+
+
+            //string sFecha = "10/05/2020 15:10:26";
+            //DateTime dateconvert = Convert.ToDateTime(sFecha);
+            //Console.WriteLine(dateconvert);
+            //TimeSpan CurrentTime = DateTime.Now.TimeOfDay;
+            //Console.WriteLine(CurrentTime);
+            //Console.WriteLine(Convert.ToDateTime("12:14:30.2084468"));
+            //DateTime finalTime = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+            //Console.WriteLine(finalTime + " date final");
+            //Console.WriteLine(finalTime.TimeOfDay + " TimeSpan");
+            //TimeSpan oTS = new TimeSpan(finalTime.Ticks);
+            //Console.WriteLine(oTS + " TimeSpan final");
+
+            //Console.WriteLine(DateTime.Now.TimeOfDay + " +24");           
+
+            //Console.WriteLine(DateTime.Now.ToLocalTime());
 
             Console.ReadLine();
            
